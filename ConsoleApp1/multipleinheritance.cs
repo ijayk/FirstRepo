7dfd7ad61e700056a000e2853ff28d534464194f
Jayanta Chakravorty <chakravorty.jayanta@rediffmail.com>
2018-06-26 02:30:10 +0530

    3rd commit

---
+++ ConsoleApp1/multipleinheritance.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multipleinheritance
+{
+    class A: IA
+    {
+        public void printA()
+        {
+            Console.WriteLine("PrintA");
+        }
+    }
+
+    interface IA
+    {
+        void printA();
+    }
+    class B
+    {
+        public void printB()
+        {
+            Console.WriteLine("PrintB");
+        }
+    }
+
+    interface IB
+    {
+        void printB();
+    }
+
+    class C: IA,IB
+    {
+       
+            A a = new A();
+            B b = new B();
+
+            public void printA()
+            {
+                a.printA();
+            }
+
+            public void printB()
+            {
+                b.printB();
+            }
+        
+        
+
+
+    }
+}
